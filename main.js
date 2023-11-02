db9c40501e05b6172962c49a56cd4cfa3b2b3a9f
Tom Swisher <tomswisherlabs@gmail.com>
2023-11-02 00:41:27 -0700

    Added basic lamp and smoke

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -16,18 +21,59 @@ const points = [];
 points.push( new THREE.Vector3( -10, 0, 0 ) );
 points.push( new THREE.Vector3( 0, 10, 0 ) );
 points.push( new THREE.Vector3( 10, 0, 0 ) );
-
 const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
 const line = new THREE.Line( geometry, material );
-
 scene.add( line );
+scene.add(new THREE.AmbientLight(0x444444));
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+scene.add(new THREE.HemisphereLight(0xffffff, 0x444444));
+
+// Load GLTF lamp model by Kay Lousberg
+let lamp;
+const loader = new GLTFLoader();
+loader.load('lamp.gltf', (gltf) => {
+  lamp = gltf.scene;
+  lamp.scale.set(10, 10, 10);
+  scene.add(lamp);
+  console.log(lamp);
+});
+
+const particleGeometry = new THREE.BufferGeometry();
+const particleCount = 10000;
+const posArray = new Float32Array(particleCount * 3);
+for (let i = 0; i < particleCount * 3; i++) {
+  posArray[i] = (Math.random() - 0.5) * 50;
+}
+particleGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
+const particleMaterial = new THREE.PointsMaterial({
+  size: 0.005,
+  transparent: true,
+});
+const particleSystem = new THREE.Points(particleGeometry, particleMaterial);
+scene.add(particleSystem);
 
 function animate() {
   requestAnimationFrame( animate );
+  controls.update();
   line.rotation.x += 0.01;
   line.rotation.y += 0.005;
   line.rotation.z -= 0.01;
+  if (lamp) {
+    lamp.rotation.y += 0.005;
+  }
+  posArray.forEach((val, idx) => {
+    const i = idx % 3;
+    if (i === 0) {
+      posArray[idx] += 0.01;
+    }
+    if (i === 1) {
+      posArray[idx] += 0.01;
+    }
+  });
+  particleSystem.geometry.attributes.position.needsUpdate = true;
   renderer.render( scene, camera );
 }
 
