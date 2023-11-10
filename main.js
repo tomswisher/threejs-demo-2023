198421e4be51496ef63767dbd06f2f056f027181
Tom Swisher <tomswisherlabs@gmail.com>
2023-11-10 00:07:48 -0800

    Added hover state and placeholder element for crystal sphere

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const camera = new THREE.PerspectiveCamera(
   1,
   500,
 );
-camera.position.set( 0, 0, 100 );
+camera.position.set( 10, 10, 100 );
 camera.lookAt( 0, 0, 0 );
 
 const scene = new THREE.Scene();
@@ -35,25 +35,33 @@ controls.enableDamping = true;
 
 scene.add(new THREE.HemisphereLight(0xffffff, 0x444444));
 
+let originalColor;
+
 // Load GLTF lamp model by Kay Lousberg
 let lamp;
 const loader = new GLTFLoader();
 loader.load('lamp.gltf', (gltf) => {
   lamp = gltf.scene;
   lamp.scale.set(10, 10, 10);
+  lamp.position.set(0, -15, 0)
   scene.add(lamp);
+  originalColor = lamp.children[0].children[0].material.color.clone();
 });
+
+let raycaster = new THREE.Raycaster();
+let pointer = new THREE.Vector2(Infinity, Infinity);
 
 const particleGeometry = new THREE.BufferGeometry();
 const particleCount = 10000;
 const posArray = new Float32Array(particleCount * 3);
-for (let i = 0; i < particleCount * 3; i++) {
-  posArray[i] = (Math.random() - 0.5) * 10;
+for (let i = 0; i < particleCount; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = Math.random() * 15;
+  posArray[3 * i + 0] = Math.cos(angle) * radius;
+  posArray[3 * i + 1] = (Math.random() - 0.5) * 30 - 15;
+  posArray[3 * i + 2] = Math.sin(angle) * radius;
 }
 particleGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
-// const particleMaterial = new THREE.PointsMaterial({
-//   size: 0.5,
-// });
 const particleMaterial = new THREE.ShaderMaterial({
     uniforms: {
         uTime: { value: 0 },
@@ -64,7 +72,7 @@ const particleMaterial = new THREE.ShaderMaterial({
         void main() {
             vUv = uv;
             vec4 mvPosition = modelViewMatrix * vec4(position, 1.);
-            // gl_PointSize = 2. * (1. / -mvPosition.z);
+            // gl_PointSize = 10. * (1. / -mvPosition.z);
             gl_PointSize = 3.;
             gl_Position = projectionMatrix * mvPosition;
         }
@@ -82,26 +90,48 @@ const particleMaterial = new THREE.ShaderMaterial({
     depthWrite: false,
     blending: THREE.AdditiveBlending
 });
-
 const particleSystem = new THREE.Points(particleGeometry, particleMaterial);
 scene.add(particleSystem);
+
+const firmament = new THREE.Mesh(
+  new THREE.SphereGeometry(30, 32, 32),
+  new THREE.MeshBasicMaterial(),
+);
+firmament.material.color.set('darkblue');
+firmament.material.side = THREE.BackSide;
+scene.add(firmament);
+
+window.addEventListener('mousemove', (e) => {
+  pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = (e.clientY / window.innerHeight) * 2 - 1;
+});
 
 function animate() {
   requestAnimationFrame( animate );
   controls.update();
-  // line.rotation.x += 0.01;
-  // line.rotation.y += 0.005;
-  // line.rotation.z -= 0.01;
   if (lamp) {
     lamp.rotation.y += 0.005;
+    raycaster.setFromCamera(pointer, camera);
+    const intersects = raycaster.intersectObject(lamp, true);
+    const mesh = lamp.children[0].children[0];
+    const lampIntersected = intersects.some((intersect) => intersect.object === mesh);
+    if (lampIntersected) {
+      originalColor = originalColor || mesh.material.color.clone();
+      console.log(originalColor);
+      mesh.material.color.set('white');
+    } else {
+      if (originalColor) {
+        mesh.material.color.copy(originalColor);
+      }
+    }
   }
   posArray.forEach((val, idx) => {
     const i = idx % 3;
     if (i === 1) {
       posArray[idx] += Math.random() * 0.4;
-    }
-    if (posArray[idx] > 30) {
-      posArray[idx] = 0;
+      if (posArray[idx] > 25) {
+        posArray[idx] = -25;
+      }
     }
   });
   particleSystem.geometry.attributes.position.needsUpdate = true;
